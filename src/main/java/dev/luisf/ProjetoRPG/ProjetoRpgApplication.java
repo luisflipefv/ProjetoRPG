af176dc96d8134fe7fc35a21c75bb22d3f0d6f68
luisf <luisfelipefv8@hotmail.com>
2025-05-23 13:14:24 -0300

    first commit

---
+++ src/main/java/dev/luisf/ProjetoRPG/ProjetoRpgApplication.java
@@ -0,0 +1,13 @@
+package dev.luisf.ProjetoRPG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoRpgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/luisf/ProjetoRPG/ProjetoRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luisf.ProjetoRPG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRpgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
