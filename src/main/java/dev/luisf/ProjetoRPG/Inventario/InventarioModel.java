fd022ea1530d2be5d119b83a440836c2ce9478f0
luisf <luisfelipefv8@hotmail.com>
2025-05-23 13:20:41 -0300

    Cria entidade Inventario

---
+++ src/main/java/dev/luisf/ProjetoRPG/Inventario/InventarioModel.java
@@ -0,0 +1,34 @@
+package dev.luisf.ProjetoRPG.Inventario;
+
+import dev.luisf.ProjetoRPG.Item.ItemModel;
+import dev.luisf.ProjetoRPG.Personagem.PersonagemModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_inventario_personagem")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class InventarioModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "capacidade_maxima")
+    private int capacidadeMaxima;
+
+    @OneToOne (fetch = FetchType.LAZY)
+    @JoinColumn(name = "personagem_id")
+    private PersonagemModel personagem;
+
+    @OneToMany(mappedBy = "inventario", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn
+    private List<ItemModel> itens = new ArrayList<>();
+}
